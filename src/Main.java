27d418e20d87334c633bf83a456cdf6460079aa9
Tanya <dasharomanova836@gmail.com>
2024-03-03 20:37:07 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Time time1 = new Time(10);
-        Time time2 = new Time(10000);
-        Time time3 = new Time(100000);
+        Time time1 = new Time(10000);
+        Time time2 = new Time(2, 3, 5);
 
-        System.out.println("10 секунд: " + time1.getTimeString());
-        System.out.println("10000 секунд: " + time2.getTimeString());
-        System.out.println("100000 секунд: " + time3.getTimeString());
+        System.out.println("10000 секунд: " + time1.getTimeString());
+        System.out.println("2 часа, 3 минуты, 5 секунд: " + time2.getTimeString());
     }
 }
--- src/Time.java
+++ src/Time.java
@@ -1,14 +1,21 @@
 public class Time {
     private int seconds;
-    public Time(int seconds) {
-        this.seconds = seconds % 86400; // учитываем только время с начала последних суток
+    public Time(int value) {
+        if (value >= 86400) {
+            this.seconds = value % 86400;
+        } else {
+            this.seconds = value;
+        }
+    }
+    public Time(int hours, int minutes, int seconds) {
+        this.seconds = hours * 3600 + minutes * 60 + seconds;
     }
 
-    public String getTimeString() {
-        int hours = seconds / 3600;
-        int minutes = (seconds % 3600) / 60;
-        int sec = seconds % 60;
+        public String getTimeString() {
+            int hours = seconds / 3600;
+            int minutes = (seconds % 3600) / 60;
+            int sec = seconds % 60;
 
-        return hours + ":" + minutes + ":" + sec;
-    }
+            return hours + ":" + minutes + ":" + sec;
+        }
 }
