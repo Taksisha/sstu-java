cb4bcc354b459d6383ced35f2cbce95e4032fb28
Tanya <dasharomanova836@gmail.com>
2024-03-03 20:41:11 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-    Human kleopatra = new Human("Клеопатра");
-    Human pushkin = new Human("Александр", "Сергеевич", "Пушкин");
-    Human maiakovski = new Human("Владимир", "Маяковский");
+        Human kleopatra = new Human("Клеопатра");
+        Human pushkin = new Human("Александр", "Сергеевич", "Пушкин");
+        Human maiakovski = new Human("Владимир", "Маяковский");
+        Human hristofor = new Human("Христофор", "Бонифатьевич");
 
-    System.out.println(kleopatra.toString());
-    System.out.println(pushkin.toString());
-    System.out.println(maiakovski.toString());
+        System.out.println(kleopatra.toString());
+        System.out.println(pushkin.toString());
+        System.out.println(maiakovski.toString());
+        System.out.println(hristofor.toString());
     }
+
 }
