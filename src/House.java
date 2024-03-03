2ace3b22547ed7efa5940058879114f09653d59c
Tanya <dasharomanova836@gmail.com>
2024-03-03 20:25:54 +0300

    1

---
+++ src/House.java
@@ -0,0 +1,17 @@
+public class House {
+    private int floors;
+
+    public House(int floors){
+        this.floors = floors;
+    }
+    public String toString() {
+        if (floors % 10 == 1 && floors % 100 != 11) {
+            return "дом с " + floors + " этажем";
+//Если количество этажей заканчивается на числа от 2 до 4, но не попадает в исключения 10-20 (например, 2-4, 22-24, 32-34 и т.д.)
+        } else if (floors % 10 >= 2 && floors % 10 <= 4 && (floors % 100 < 10 || floors % 100 >= 20)) {
+            return "дом с " + floors + " этажами";
+        } else {
+            return "дом с " + floors + " этажами";
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        House house1 = new House(1);
+        House house2 = new House(35);
+        House house3 = new House(91);
 
+        System.out.println(house1.toString());
+        System.out.println(house2.toString());
+        System.out.println(house3.toString());
     }
 }
