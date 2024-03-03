80d7aedd516d984a31ceb2c9d157f6611377fd61
KirillTheARCHIK <kirillthearchik@yandex.ru>
2024-03-03 10:04:44 +0400

    i

---
+++ src/City.java
@@ -0,0 +1,15 @@
+public class City {
+    int point;
+    String name;
+    arrayCity[] arrayCity;
+
+    City(){
+        this.name = name;
+        this.point = point;
+        this.arrayCity = arrayCity;
+    }
+
+    public String toString(){
+
+    }
+}
---
+++ src/arrayCity.java
@@ -0,0 +1,12 @@
+public class arrayCity {
+    City name;
+    int cost;
+
+    arrayCity(){
+        this.name = name;
+        this.cost = cost;
+    }
+
+    @Override
+
+}
