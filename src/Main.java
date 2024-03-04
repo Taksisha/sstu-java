7e9a7f171166a4a6ff4bec842a21329637df370e
Tanya <dasharomanova836@gmail.com>
2024-03-04 09:18:45 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Human kleopatra = new Human("Клеопатра", 152);
+        Human pushkin = new Human("Пушкин", 167);
+        Human vladimir = new Human("Владимир", 189);
 
+        System.out.println(kleopatra.toString());
+        System.out.println(pushkin.toString());
+        System.out.println(vladimir.toString());
     }
 }
