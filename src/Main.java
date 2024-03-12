7dc1f6ddd3112e925f577a2f4d7e7c10a7c65d00
Tanya <dasharomanova836@gmail.com>
2024-03-12 13:28:12 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] vasyaGrades = {3, 4, 5};
+        int[] vasyaGrades = {3, 4, 5, 4};
         Student vasya = new Student("Вася", vasyaGrades);
-        Student maxim = new Student("Максим");
+        Student petya = new Student("Петя", 5, 5, 5, 5);
 
-        System.out.println(vasya);
-        System.out.println(maxim);
+        System.out.println("Вася: средний балл - " + vasya.getAverageGrade() + ", отличник - " + vasya.isExcellentStudent());
+        System.out.println("Петя: средний балл - " + petya.getAverageGrade() + ", отличник - " + petya.isExcellentStudent());
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -11,6 +11,29 @@ class Student {
         }
     }
 
+    public double getAverageGrade() {
+        if (grades.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.length;
+    }
+
+    public boolean isExcellentStudent() {
+        if (grades.length == 0) {
+            return false;
+        }
+        for (int grade : grades) {
+            if (grade != 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
