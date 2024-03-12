c8dcfcde8ad01915c109d1bb50539492c216b35a
Tanya <dasharomanova836@gmail.com>
2024-03-12 13:13:38 +0300

    1

---
+++ src/Fraction.java
@@ -0,0 +1,53 @@
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction subtract(Fraction other) {
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction add(int num) {
+        return new Fraction(this.numerator + num * this.denominator, this.denominator);
+    }
+
+    public Fraction subtract(int num) {
+        return new Fraction(this.numerator - num * this.denominator, this.denominator);
+    }
+
+    public Fraction multiply(int num) {
+        return new Fraction(this.numerator * num, this.denominator);
+    }
+
+    public Fraction divide(int num) {
+        return new Fraction(this.numerator, this.denominator * num);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Fraction f1 = new Fraction(1, 3);
+        Fraction f2 = new Fraction(2, 3);
+        Fraction f3 = new Fraction(3, 4);
 
+        System.out.println(f1 + " * " + f2 + " = " + f1.multiply(f2));
+        System.out.println(f1 + " + " + f2 + " = " + f1.add(f2));
+        System.out.println(f1 + " - " + f2 + " = " + f1.subtract(f2));
+        System.out.println(f1 + " / " + f2 + " = " + f1.divide(f2));
+
+        Fraction result = f1.add(f2).divide(f3).subtract(5);
+        System.out.println(f1 + " + " + f2 + " / " + f3 + " - 5 = " + result);
     }
 }
