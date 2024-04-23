6c9fa5ce8de1648991a3adc9935f637c1e09d3e2
Tanya <dasharomanova836@gmail.com>
2024-04-24 02:25:50 +0400

    1

---
+++ src/ru/obydennova/student/Comparable.java
@@ -0,0 +1,86 @@
+package ru.obydennova.student;
+
+public class Student implements Comparable<Student> {
+    String name;
+    int[] grades;
+
+    public Student(String name, int... grades) {
+        this.name = name;
+        if (grades != null) {
+            ArrayList<Integer> validGrades = new ArrayList<>();
+            for (int grade : grades) {
+                if (grade >= 2 && grade <= 5) {
+                    validGrades.add(grade);
+                }
+            }
+            this.grades = validGrades.stream().mapToInt(Integer::intValue).toArray();
+        } else {
+            this.grades = new int[0];
+        }
+    }
+
+    public double getAverageGrade() {
+        if (grades.length == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+        return (double) sum / grades.length;
+    }
+
+    public boolean isExcellentStudent() {
+        if (grades.length == 0) {
+            return false;
+        }
+        for (int grade : grades) {
+            if (grade != 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name).append(": [");
+        for (int i = 0; i < grades.length; i++) {
+            sb.append(grades[i]);
+            if (i != grades.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Student otherStudent) {
+        double thisAverage = this.getAverageGrade();
+        double otherAverage = otherStudent.getAverageGrade();
+        if (thisAverage > otherAverage) {
+            return 1;
+        } else if (thisAverage < otherAverage) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 4, 5, 5);
+        Student student2 = new Student("Bob", 3, 4, 4);
+
+        int comparison = student1.compareTo(student2);
+
+        if (comparison > 0) {
+            System.out.println(student1.name + " has a higher average grade.");
+        } else if (comparison < 0) {
+            System.out.println(student2.name + " has a higher average grade.");
+        } else {
+            System.out.println("Both students have the same average grade.");
+        }
+    }
+}
+
